25f249b4d9555ff86a45a61ee45a8abe4cfc6e66
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-03-07 16:38:24 +0800

    add users endpoint

---
+++ users.py
@@ -0,0 +1,59 @@
+from flask import jsonify, Blueprint, request
+import psycopg2
+
+users_bp = Blueprint("users", __name__)
+
+def get_connection():
+    return psycopg2.connect(
+        host = 'localhost',
+        database = 'gabay_gov',
+        user = 'jann',
+        password = '@dmin098')
+
+get_connection()
+
+
+@users_bp.route('/register', methods = ['POST'])
+def register():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    data = request.json
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+    government_id = data.get ('government_id')
+    
+    cur.execute ('''INSERT INTO organization_users 
+                 (first_name, last_name, email, username, password, government_id) 
+                 VALUES (%s, %s, %s, %s, %s, %s)''',
+                 (first_name, last_name,email,username,password,government_id))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return ({"Success":"User Added Succesfully"}), 200
+
+
+@users_bp.route('/login', methods = ['POST'])
+def login():
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    data=request.json
+    username = data.get('username')
+    password = data.get('password')
+        
+    cur.execute('''SELECT id, username, password 
+                FROM organization_users 
+                WHERE username=%s AND password = %s;''', (username, password))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    if user:
+        return jsonify({"Success":"Login Success"}), 200
+    
+    return jsonify({"Failed": "Login Failed"}), 422
