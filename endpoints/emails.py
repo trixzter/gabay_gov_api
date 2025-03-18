f6674395487c7ab03e9a089d9f1299df3b62cf75
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-03-18 09:47:26 +0800

    updated the spacing of all the code

--- endpoints/emails.py
+++ endpoints/emails.py
@@ -1,18 +1,16 @@
 from flask import Blueprint, jsonify, request
-from dao.email_dao import create_email, check_email
+from dao.email_dao import create_email, email_existing
 
-emails_bp = Blueprint ('emails', __name__)
+emails_bp = Blueprint('emails', __name__)
 
 
 @emails_bp.route('', methods=['POST'])
-def add_email ():
-    data=request.json
+def add_email():
+    data = request.json
     subscriber_email = data.get('subscriber_email')
-    
-    email_exist = check_email(subscriber_email)
 
-    if email_exist:
-        return jsonify ({"Error":"Email Already Exist"}), 422
+    if email_existing(subscriber_email):
+        return jsonify({"Error":"Email Already Exist"}), 422
 
     create_email(subscriber_email)
-    return jsonify ({"Success":"Email Added Successfully"}), 200
+    return jsonify({"Success":"Email Added Successfully"}), 200
