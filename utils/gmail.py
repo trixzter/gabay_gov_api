3591939bbd4d2854883d88288a0cbc463e18ee54
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-03-18 21:41:33 +0800

    added create_event_notification that sends emails to users once an event is created

--- utils/gmail.py
+++ utils/gmail.py
@@ -31,6 +31,7 @@ def event_notification():
   EMAIL_PASSWORD = os.getenv('EMAIL_PASS')
 
   with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
     subject = 'EVENT NOTIFICATION'
@@ -47,6 +48,31 @@ def event_notification():
         smtp.sendmail(EMAIL_ADDRESS, [recipient_email], msg)
 
 
-if __name__ == '__main__':
-  # reset_password()
-  event_notification()
+def create_event_notification(title, description):
+  dotenv.load_dotenv()
+  
+  EMAIL_ADDRESS = os.getenv('EMAIL_USER')
+  EMAIL_PASSWORD = os.getenv('EMAIL_PASS')
+
+  with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+  subject = title
+  body = description
+  msg = f'Subject: {subject}\n\n{body}'
+
+  email_tuples = email_dao.get_emails()
+    
+  email_list = [email_tuple[0] for email_tuple in email_tuples]
+
+  if email_list:
+      with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+          smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+          for recipient_email in email_list:
+            smtp.sendmail(EMAIL_ADDRESS, recipient_email, msg)
+
+            
+# if __name__ == '__main__':
+#   # reset_password()
+#   event_notification()
