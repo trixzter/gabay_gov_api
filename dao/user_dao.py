63ec1165a2d5c9230a14203bd46d348b3d17e627
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-03-18 09:45:58 +0800

    added typehints to function parameters, renamed functions readability

--- dao/user_dao.py
+++ dao/user_dao.py
@@ -1,11 +1,11 @@
 from init_db import get_connection
 
 
-def register_user (first_name, last_name, email, username, password, government_id):
+def create_user(first_name:str, last_name:str, email:str, username:str, password:str, government_id:str):
   conn = get_connection()
   cur = conn.cursor()
 
-  cur.execute ('''INSERT INTO organization_users 
+  cur.execute('''INSERT INTO organization_users 
               (first_name, last_name, email, username, password, government_id)
               VALUES (%s, %s, %s, %s, %s, %s)''',
               (first_name, last_name, email, username, password, government_id))
@@ -14,14 +14,12 @@ def register_user (first_name, last_name, email, username, password, government_
   conn.close()
 
 
-def login_user ( username, password):
+def login_user(username:str, password:str):
   conn = get_connection()
   cur = conn.cursor()
 
-  cur.execute ('''SELECT first_name, last_name, email, username
-              FROM organization_users 
-              WHERE username = %s AND password = %s''', 
-              (username, password))
+  cur.execute('''SELECT first_name, last_name, email, username FROM organization_users 
+              WHERE username = %s AND password = %s''', (username, password))
   user = cur.fetchone()
   cur.close()
   conn.close()
@@ -29,27 +27,23 @@ def login_user ( username, password):
   return user
 
 
-def update_user (id, first_name, last_name, email, username, password, government_id):
-  conn = get_connection ()
+def update_user(id:int, first_name:str, last_name:str, email:str, username:str, password:str, government_id:str):
+  conn = get_connection()
   cur = conn.cursor()
 
-  cur.execute ('''UPDATE organization_users 
+  cur.execute('''UPDATE organization_users 
                 SET first_name = %s, last_name = %s, email = %s, username = %s, password = %s, government_id = %s 
-                WHERE id = %s''',
-                (first_name, last_name, email, username, password, government_id, id))
+                WHERE id = %s''', (first_name, last_name, email, username, password, government_id, id))
   conn.commit()
   cur.close()
   conn.close()
 
 
-def check_user (id):
+def check_user_dao(id:int):
   conn = get_connection()
   cur = conn.cursor()
   
-  cur.execute('''SELECT id 
-              FROM organization_users 
-              WHERE id=%s''', 
-              (id,))
+  cur.execute('SELECT id FROM organization_users WHERE id = %s', (id,))
   user_check = cur.fetchone() is not None
   cur.close()
   conn.close()
