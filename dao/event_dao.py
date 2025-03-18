64a64675ba015870665dbb5777c1939bc4ba530d
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-03-18 09:45:43 +0800

    added typehints to function parameters, renamed functions better readability

--- dao/event_dao.py
+++ dao/event_dao.py
@@ -2,12 +2,11 @@ from init_db import get_connection
 from psycopg2.extras import RealDictCursor
 
 
-def create_event(title, date, time, location, photo, description):
+def create_event(title:str, date:str, time:str, location:str, photo:str, description:str):
   conn = get_connection()
   cur = conn.cursor(cursor_factory=RealDictCursor)
 
-  cur.execute('''
-                INSERT INTO events (title, date, time, location, photo, description)
+  cur.execute('''INSERT INTO events (title, date, time, location, photo, description)
               VALUES (%s, %s, %s, %s, %s, %s)''',
               (title, date, time, location, photo, description))
   conn.commit()
@@ -15,7 +14,7 @@ def create_event(title, date, time, location, photo, description):
   conn.close()
 
 
-def view_all_events(title, location):
+def get_events(title:str, location:str):
   conn = get_connection()
   cur = conn.cursor(cursor_factory=RealDictCursor)
     
@@ -43,55 +42,46 @@ def view_all_events(title, location):
   return events
   
 
-def view_event (id):
+def get_event_dao(id:int):
   conn = get_connection()
   cur = conn.cursor(cursor_factory=RealDictCursor)
 
-  cur.execute('''SELECT * 
-              FROM events 
-              WHERE id = %s''', 
-              (id,))
-  event = cur.fetchone()
+  cur.execute('SELECT * FROM events WHERE id = %s', (id,))
+  event_check = cur.fetchone()
 
   cur.close()
   conn.close()
-  return event
+  return event_check
 
 
-def edit_event (id, title, date, time, location, photo, description):
+def update_event(id:int, title:str, date:str, time:str, location:str, photo:str, description:str):
   conn = get_connection()
   cur = conn.cursor()
 
   cur.execute('''UPDATE events 
                 SET title = %s, date = %s, time = %s, location = %s, photo = %s, description = %s
-                WHERE id = %s''',
-                (title, date, time, location, photo, description, id))
+                WHERE id = %s''', (title, date, time, location, photo, description, id))
   conn.commit()
   cur.close()
   conn.close()
 
 
-def remove_event(id):
+def delete_event(id:int):
   conn = get_connection()
   cur = conn.cursor()
 
-  cur.execute('''DELETE FROM events 
-                WHERE id = %s''', 
-                (id,))
+  cur.execute('DELETE FROM events WHERE id = %s', (id,))
   conn.commit()
   cur.close()
   conn.close()
 
 
-def check_event(id):
+def check_event_dao(id:int):
   conn = get_connection()
   cur = conn.cursor()
 
-  cur.execute ('''SELECT * 
-               FROM events 
-               WHERE id = %s''', 
-               (id,))
-  event = cur.fetchone() is not None
+  cur.execute('SELECT * FROM events WHERE id = %s', (id,))
+  event_check = cur.fetchone() is not None
   cur.close()
   conn.close()
-  return event
+  return event_check
