397b9161b6171f6cc05bddc57d0dc91db339eaf8
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-03-18 09:43:43 +0800

    renamed functions for better readability, added typehints

--- dao/email_dao.py
+++ dao/email_dao.py
@@ -1,27 +1,21 @@
 from init_db import get_connection
 
 
-def create_email(subscriber_email):
+def create_email(subscriber_email:str):
   conn = get_connection()
   cur = conn.cursor()
 
-  cur.execute ('''INSERT INTO subscription_emails 
-                (subscriber_email)
-                VALUES (%s)''',
-                (subscriber_email,))
+  cur.execute('INSERT INTO subscription_emails (subscriber_email) VALUES (%s)', (subscriber_email,))
   conn.commit()
   cur.close()
   conn.close()
-      
 
-def check_email(subscriber_email):
+
+def email_existing(subscriber_email:str):
   conn = get_connection()
   cur = conn.cursor()
 
-  cur.execute('''SELECT 1 
-              FROM subscription_emails 
-              WHERE subscriber_email = %s''',
-              (subscriber_email,))
+  cur.execute('SELECT 1 FROM subscription_emails WHERE subscriber_email = %s',(subscriber_email,))
   email_exist = cur.fetchone() is not None 
   cur.close()
   conn.close()
